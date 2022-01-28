dd7dc1886cfafd6c81c6f0d889b77e451ca1a56f
Markus <markus.skau@gmail.com>
2022-01-28 18:29:07 +0100

    changed values

--- myquote/myquote.go
+++ myquote/myquote.go
@@ -3,5 +3,5 @@ package myquote
 import "rsc.io/quote"
 
 func TestQuote() string {
-	return quote.Hello() + "\n" + quote.Go() + "\n" + quote.Glass() + "\n" + quote.Opt()
+	return quote.Hello() + "\n" + quote.Go() + "\n" + quote.Opt() + "\n" + quote.Glass()
 }
